c5f429a0258183d1637ab3607a9dd400164ad500
burade09 <pranayburade9@gmail.com>
2022-07-11 15:22:43 +0530

    second commit

---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Http\Controllers\TaskController;
+use Illuminate\Support\Facades\Route;
+
+
+// Route::get('/','TaskController@index');
+Route::get('/',[TaskController::class,'index']);
+Route::post('/task/create',[TaskController::class,'store'])->name('task.create');
+Route::delete('/task/delete/{task}',[TaskController::class,'destroy'])->name('task.destroy');
